34e379a0e20e9e6f65353ffe5561e45bdfe364f8
Christy Nicholson <christy.nicholson@gmail.com>
2023-12-14 21:15:07 -0500

    Nov 2023 Class SPA

---
+++ store/Pizza.js
@@ -0,0 +1,4 @@
+export default {
+  header: "My Pizza Page",
+  view: "Pizza"
+};
---
+++ store/index.js
@@ -0,0 +1,6 @@
+export { default as Bio } from "./Bio";
+export { default as Home } from "./Home";
+export { default as Order } from "./Order";
+export { default as Pizza } from "./Pizza";
+export { default as Viewnotfound } from "./Viewnotfound";
+export { default as Links } from "./Links";
